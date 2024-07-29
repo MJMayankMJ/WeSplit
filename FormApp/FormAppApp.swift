2fa16bdc36f29783c1709b6801b7cb8918fa47de
MJMayankMJ <226301131@gkv.ac.in>
2024-07-29 17:30:44 +0530

    Initial Commit

---
+++ FormApp/FormAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormAppApp.swift
+//  FormApp
+//
+//  Created by Mayank Jangid on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct FormAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
